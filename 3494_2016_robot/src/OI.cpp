9592e70208b3435c4563115b5967512586dff450
Caleb Berger <tkdberger@gmail.com>
2016-02-12 20:08:11 -0500

    Added controller things to OI

--- 3494_2016_robot/src/OI.cpp
+++ 3494_2016_robot/src/OI.cpp
@@ -3,4 +3,27 @@
 OI::OI()
 {
 	// Process operator interface input here.
+	//Define controllers as Joystick objects
+	controller = new Joystick(0);
+	controller_2 = new Joystick(1);
+
+	//Define all the buttons, all of them
+	buttonA = new JoystickButton(controller, 1);
+	buttonB = new JoystickButton(controller, 2);
+	buttonX = new JoystickButton(controller, 3);
+	buttonY = new JoystickButton(controller, 4);
+	buttonLB = new JoystickButton(controller, 5);
+	buttonRB = new JoystickButton(controller, 6);
+	buttonStart = new JoystickButton(controller, 8);
+	buttonSelect = new JoystickButton(controller, 7);
+
+	buttonA_2 = new JoystickButton(controller_2, 2);//2  //  xbox or whats
+	buttonB_2 = new JoystickButton(controller_2, 3);
+	buttonX_2 = new JoystickButton(controller_2, 1);
+	buttonY_2 = new JoystickButton(controller_2, 4);
+	buttonLB_2 = new JoystickButton(controller_2, 5);
+	buttonRB_2 = new JoystickButton(controller_2, 6);
+	buttonStart_2 = new JoystickButton(controller_2, 8);
+	buttonSelect_2 = new JoystickButton(controller_2, 7);
+
 }
--- 3494_2016_robot/src/OI.h
+++ 3494_2016_robot/src/OI.h
@@ -6,8 +6,26 @@
 class OI
 {
 private:
-
+	Joystick* controller;
+	Joystick* controller_2;
 public:
+	JoystickButton* buttonA;
+	JoystickButton* buttonB;
+	JoystickButton* buttonX;
+	JoystickButton* buttonY;
+	JoystickButton* buttonLB;
+	JoystickButton* buttonRB;
+	JoystickButton* buttonStart;
+	JoystickButton* buttonSelect;
+
+	JoystickButton* buttonA_2;
+	JoystickButton* buttonB_2;
+	JoystickButton* buttonX_2;
+	JoystickButton* buttonY_2;
+	JoystickButton* buttonLB_2;
+	JoystickButton* buttonRB_2;
+	JoystickButton* buttonStart_2;
+	JoystickButton* buttonSelect_2;
 	OI();
 };
 
