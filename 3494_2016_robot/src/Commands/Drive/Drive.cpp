292675a8d7eff4070198b824126d79bd3b335b60
Caleb Berger <tkdberger@gmail.com>
2016-02-15 18:09:25 -0500

    2015 code FTW!
    2015 code FTW!
    2015 code FTW!
    2015 code FTW!
    2015 code FTW!

--- 3494_2016_robot/src/Commands/Drive/Drive.cpp
+++ 3494_2016_robot/src/Commands/Drive/Drive.cpp
@@ -15,9 +15,24 @@ void Drive::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void Drive::Execute()
 {
+	int dpad = oi->GetDPad();
 	float leftThrottle =oi->GetLeftJoystick();
 	float rightThrottle =oi->GetRightJoystick();
 	CommandBase::driveTrain->TankDrive(leftThrottle, rightThrottle);
+
+	//dpad steering
+	if (dpad == 0){
+		CommandBase::driveTrain->TankDrive(-0.5, 0.5);
+	}
+	else if (dpad == 180){
+		CommandBase::driveTrain->TankDrive(0.5, -0.5);
+	}
+	else if (dpad == 90){
+		CommandBase::driveTrain->TankDrive(-0.5, -0.5);
+	}
+	else if (dpad == 270){
+		CommandBase::driveTrain->TankDrive(0.5, 0.5);
+	}
 }
 
 // Make this return true when this Command no longer needs to run execute()
