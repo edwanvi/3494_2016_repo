4790e131cbb73398ab9845353ff4b57ee9445628
Caleb Berger <tkdberger@gmail.com>
2016-03-04 19:28:46 -0500

    ACTUALLY added said command

---
+++ 3494_2016_robot/src/Commands/CommandWinch.cpp
@@ -0,0 +1,48 @@
+#include "CommandWinch.h"
+#include "../OI.h"
+
+CommandWinch::CommandWinch()
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+	Requires(CommandBase::climber);
+}
+
+// Called just before this Command runs the first time
+void CommandWinch::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void CommandWinch::Execute()
+{
+	if (oi->GetDPad_2() == 0){
+		climber->Winch(true);
+	}
+	else if (oi->GetDPad_2() == 180){
+		climber->Winch(false);
+	}
+	else {
+		climber->StopWinch();
+	}
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool CommandWinch::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void CommandWinch::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void CommandWinch::Interrupted()
+{
+
+}
